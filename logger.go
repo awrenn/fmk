b5a196ca97e9289bf1dfc4d08ddac8d13b47913d
Andrew Wrenn <awrenn53@gmail.com>
2018-09-19 08:53:32 -0700

    Adding ability to change writer objects for the logger object

---
+++ logger.go
@@ -0,0 +1,51 @@
+package fmk
+
+import (
+	"io"
+	"log"
+)
+
+type Logger struct {
+	Trace   *log.Logger
+	Info    *log.Logger
+	Warning *log.Logger
+	Error   *log.Logger
+}
+
+var Log Logger
+
+func LogInit(
+	traceHandle io.Writer,
+	infoHandle io.Writer,
+	warningHandle io.Writer,
+	errorHandle io.Writer) {
+
+	Log.SetTrace(traceHandle)
+	Log.SetInfo(infoHandle)
+	Log.SetWarning(warningHandle)
+	Log.SetError(errorHandle)
+}
+
+func (logger *Logger) SetTrace(w io.Writer) {
+	logger.Trace = log.New(w,
+		"TRACE: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func (logger *Logger) SetInfo(w io.Writer) {
+	logger.Info = log.New(w,
+		"INFO: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func (logger *Logger) SetWarning(w io.Writer) {
+	logger.Warning = log.New(w,
+		"WARNING: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func (logger *Logger) SetError(w io.Writer) {
+	logger.Error = log.New(w,
+		"ERROR: ",
+		log.Ldate|log.Ltime|log.Lshortfile)
+}
