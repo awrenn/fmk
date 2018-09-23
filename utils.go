7221f06a0ebb23a38346e34fb81c8b2ebb7b3eb4
Andrew Wrenn <awrenn53@gmail.com>
2018-09-23 15:37:07 -0700

    exporting ParseURL

--- utils.go
+++ utils.go
@@ -14,6 +14,10 @@ var (
 	CipherTextTooShort error = errors.New("FMK: Ciphertext too short")
 )
 
+const (
+        FRONT_SLASH = 47 // literally ascii/utf8 for '/'
+)
+
 // This is constant with respect to N, too much crypto packages messing with my head
 func equal(a []byte, b []byte) bool {
 	is_equal := true
@@ -77,4 +81,20 @@ func dearmor(end string) ([]byte, error) {
 
 func pow(a, b int) byte {
 	return byte(math.Pow(float64(a), float64(b)))
+}
+
+func ParseURL(url string) []string {
+        urlBytes := []byte(url)
+        wordBuilder := make([]byte, 0)
+        result := make([]string, 0)
+        for _, char := range urlBytes {
+                if char == FRONT_SLASH {
+                        result = append(result, string(wordBuilder))
+                        wordBuilder = make([]byte, 0)
+                } else {
+                        wordBuilder = append(wordBuilder, char)
+                }
+        }
+        result = append(result, string(wordBuilder))
+        return result
 }
